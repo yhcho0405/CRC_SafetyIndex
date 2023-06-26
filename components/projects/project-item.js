723740c5bbe35e9beaaaca0517d23c656dee629c
Dohyeon Lee <56117895+IDpluto@users.noreply.github.com>
2023-06-26 17:03:14 +0900

    Update project-item.js

--- components/projects/project-item.js
+++ components/projects/project-item.js
@@ -189,11 +189,10 @@ export default function ProjectItem({ data }) {
                 </div>
             </div>
             <div className="w-full md:w-96 md:max-w-full mx-auto">
-                <iframe 
-                    src="https://7b86-210-102-180-52.ngrok-free.app/d-solo/c8155aca-3daa-44b5-ae90-1b38bb839b9a/new-dashboard?orgId=1&from=now-1m&to=now&refresh=5s&panelId=1" 
-                        width="450" 
-                        height="200" 
-                         frameborder="0">
+                <iframe src="https://7b86-210-102-180-52.ngrok-free.app/d-solo/c8155aca-3daa-44b5-ae90-1b38bb839b9a/new-dashboard?orgId=1&from=now-1m&to=now&refresh=5s&panelId=1" 
+                    width="450" 
+                    height="200" f
+                    rameborder="0">
                 </iframe>
                 <div className="bg-white shadow-lg rounded-xl flex items-start max-w-1/2 w-[90%] lg:w-full justify-center py-4 px-4 mx-4 my-2">
                     <div className="flex items-center justify-start w-full">
