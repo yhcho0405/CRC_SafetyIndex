99a7ab0d6208823217d78eda74c62cc2bd438292
Dohyeon Lee <56117895+IDpluto@users.noreply.github.com>
2023-07-29 18:21:56 +0900

    Update project-item.js

--- components/projects/project-item.js
+++ components/projects/project-item.js
@@ -189,7 +189,7 @@ export default function ProjectItem({ data }) {
                 </div>
             </div>
             <div className="w-full md:w-96 md:max-w-full mx-auto">
-               <iframe src="http://crc-safety-index.kro.kr:3000/d-solo/dac45951-a9f0-4bc9-a473-7f013ffbb578/new-dashboard?orgId=1&from=now-24h&to=now&panelId=1" 
+               <iframe src="http://crc-safety-index.kro.kr:3000/d-solo/dac45951-a9f0-4bc9-a473-7f013ffbb578/new-dashboard?orgId=1&from=1690623399619&to=1690623975161&panelId=1" 
                     width="450" 
                     height="200" f
                     rameborder="0">
